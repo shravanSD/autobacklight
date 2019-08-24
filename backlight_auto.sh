05423ddca4c58618ad435f54d7028756f130c481
Shravan Dwarka <shravan@oceandba.com>
2019-08-24 14:15:51 +0400

    f/Issue #1
    First commit
    Modified README.md
    Pushed script

---
+++ backlight_auto.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+## Author: Shravan Dwarka (shravan_dwarka@live.com)
+## Date: 2019-08-24
+## Version: 1.0
+##
+## SET BACKLIGHT TO ON @ 18:00 (PC Time)
+## SET BACKLIGHT TO OFF @ 06 00 (PC Time)
+## SET VALUE=1 for 50%; SET VALUE=100 for 100%.
+## TIME_ON_min: The time in 24H with no ':' to set backlight ON to the minimum
+## TIME_ON_max: The time in 24H with no ':' to set backlight ON to the maximum
+## TIME_OFF: The time in 24H with no ':' to set backlight OFF
+
+bash -n "$0"
+set -e
+set -o nounset
+
+CURRENT_VALUE="$(cat /sys/class/leds/dell::kbd_backlight/brightness)"
+CURRENT_TIME="$(date '+%H%M%S')"
+TIME_ON_min=170000
+TIME_ON_max=200000
+TIME_OFF=60000
+LOG_FILE=/var/log/backlight.log
+
+if [ "$CURRENT_TIME" -gt "$TIME_OFF" ]; then
+	if [ ! "$CURRENT_VALUE" -eq 0 ]; then
+		echo 0 > /sys/class/leds/dell::kbd_backlight/brightness
+		echo "$(date +%H%M%S)": Backlight set to 0. >> $LOG_FILE
+	fi
+elif [ "$CURRENT_TIME" -gt "$TIME_ON_min" && "$CURRENT_TIME" -lt "$TIME_ON_max" ]; then
+	if [ ! "$CURRENT_VALUE" -eq 1 ]; then
+		echo 1 > /sys/class/leds/dell::kbd_backlight/brightness
+		echo "$(date +%H%M%S)": Backlight set to 1. >> $LOG_FILE
+	fi
+elif [ "$CURRENT_TIME" -gt "$TIME_ON_max" ]; then
+	if [ ! "$CURRENT_VALUE" -eq 100 ]; then
+		echo 100 > /sys/class/leds/dell::kbd_backlight/brightness
+		echo "$(date +%H%M%S)": Backlight set to 100. >> $LOG_FILE
+	fi
+fi
